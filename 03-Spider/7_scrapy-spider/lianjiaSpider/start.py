6925bd70fe4588cee58492bc26d3a2e115e8b040
lanms <1367000465@qq.com>
2018-11-13 17:24:33 +0800

    ok

--- 03-Spider/7_scrapy-spider/lianjiaSpider/start.py
+++ 03-Spider/7_scrapy-spider/lianjiaSpider/start.py
@@ -2,9 +2,9 @@
 from scrapy.cmdline import execute
 
 # 二手房
-execute(['scrapy', 'crawl', 'ershoufang'])
+# execute(['scrapy', 'crawl', 'ershoufang'])
 # 成交
-# execute(['scrapy', 'crawl', 'chenjiao'])
+execute(['scrapy', 'crawl', 'chenjiao'])
 # # # 新房 楼盘
 # execute(['scrapy', 'crawl', 'loupan'])
 # # # 租房
